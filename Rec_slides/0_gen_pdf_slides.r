8060620d51f48e13fb2df394bdbc6ef856762798
Shunkei3 <skakimoto3@huskers.unl.edu>
2024-02-15 20:51:58 -0600

    slides update

---
+++ Rec_slides/0_gen_pdf_slides.r
@@ -0,0 +1,9 @@
+library(renderthis)
+# library(xaringanBuilder)
+# build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
+build_pdf(
+	"~/Dropbox/UM/TA/apec8003_24/Gitcontrolled/Rec_slides/Rec4/Recitation4_slides.html",
+	complex_slides = TRUE, 
+	partial_slides = TRUE
+	)
+
