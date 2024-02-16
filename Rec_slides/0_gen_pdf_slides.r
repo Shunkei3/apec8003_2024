cd599d1318b7617e41bbbaeca0f1dc4b7d4c4147
Shunkei3 <skakimoto3@huskers.unl.edu>
2024-02-16 17:34:08 -0600

    update rec 5 slides

--- Rec_slides/0_gen_pdf_slides.r
+++ Rec_slides/0_gen_pdf_slides.r
@@ -2,7 +2,7 @@ library(renderthis)
 # library(xaringanBuilder)
 # build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
 build_pdf(
-	"~/Dropbox/UM/TA/apec8003_24/Gitcontrolled/Rec_slides/Rec4/Recitation4_slides.html",
+	"~/Dropbox/UM/TA/apec8003_24/Gitcontrolled/Rec_slides/Rec5/Recitation5_slides.html",
 	complex_slides = TRUE, 
 	partial_slides = TRUE
 	)
