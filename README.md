1fec58c07eb06d9d7e2c093e85b2bc0c490364b6
Shunkei3 <skakimoto3@huskers.unl.edu>
2024-03-04 10:23:48 -0600

    updates

--- README.md
+++ README.md
@@ -10,6 +10,6 @@ This is the repository for APEC 8003: Game Theory and Information at the Univers
 	+ Solution ([here](https://shunkei3.github.io/apec8003_2024/Rec_slides/Rec3/solution/Recitation3_solutions.pdf))
 + Week 4: Review - midterm exam problem 3 ([slides](https://shunkei3.github.io/apec8003_2024/Rec_slides/Rec4/Recitation4_slides.html))
 + Week 5: Dynamic game of incomplete information 1 ([slides](https://shunkei3.github.io/apec8003_2024/Rec_slides/Rec5/Recitation5_slides.html))
-+ Week 6: Dynamic game of incomplete information 2 
++ Week 6: Dynamic game of incomplete information 2 ([slides](https://shunkei3.github.io/apec8003_2024/Rec_slides/Rec6/Recitation6_slides.html))
 
 <!-- ([slides](https://shunkei3.github.io/apec8003_2024/Rec_slides/Rec5/rec5_slides.html)) -->
--- Rec_slides/0_gen_pdf_slides.r
+++ Rec_slides/0_gen_pdf_slides.r
@@ -2,8 +2,20 @@ library(renderthis)
 # library(xaringanBuilder)
 # build_pdf("~/Dropbox/UM/TA/apec8211_23/Gitcontrolled/Recitation/rec5/recitation5_slides.html")
 build_pdf(
-	"~/Dropbox/UM/TA/apec8003_24/Gitcontrolled/Rec_slides/Rec5/Recitation5_slides.html",
+	"~/Dropbox/UM/TA/apec8003_24/Gitcontrolled/Rec_slides/Rec6/Recitation6_slides.html",
 	complex_slides = TRUE, 
 	partial_slides = TRUE
 	)
 
+
+
+# render this causes errors
+
+# Error in assert_chromote() :
+#   `chromote` is required: remotes::install_github('rstudio/chromote')
+# In addition: Warning message:
+# `build_pdf()` was deprecated in renderthis 0.1.0.
+# ℹ Please use `to_pdf()` instead.
+
+# Error in to_pdf_complex(path_from(step_html, "url"), output_file, partial_slides,  :
+  # `pdftools` is required: install.packages('pdftools')
